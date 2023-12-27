ea6038a0a41ca4627b193d4047fcc873883eee12
Messi Lee <hojunlee@wustl.edu>
2023-12-27 16:51:22 -0600

    Update plot_weat.R
    ggpubr was used to place two ggplots on a single panel

--- WEAT/plot_weat.R
+++ WEAT/plot_weat.R
@@ -2,22 +2,23 @@
 ## Anonymous
 # America's Racial Framework of Superiority and Americanness Embedded in Natural Language
 
-## Script date: 4 Oct 2023
+## Script date: 27 Dec 2023
 
 # Install and load packages ----------------------------------------------------
 
 if(!require("tidyverse")){install.packages("tidyverse", dependencies = TRUE); require("tidyverse")}
 if(!require("ggplot2")){install.packages("ggplot2", dependencies = TRUE); require("ggplot2")}
+if(!require("ggpubr")){install.packages("ggpubr", dependencies = TRUE); require("ggpubr")}
 
-# Import the Dataset -----------------------------------------------------------
+# Import the dataset -----------------------------------------------------------
 
 # Import WEAT results
 setwd("Results")
 
 # Change below line of code to test different threshold values to compile names
-# weat_table = read.csv('weat_results_70.csv')
+weat_table = read.csv('weat_results_70.csv')
 # weat_table = read.csv('weat_results_60.csv')
-weat_table = read.csv('weat_results_7020.csv')
+# weat_table = read.csv('weat_results_7020.csv')
 
 # Filter WEAT results in the superiority dimension
 superior_table <- weat_table %>% 
@@ -39,12 +40,12 @@ american_table <- weat_table %>%
                                             "White v. Asian people",
                                             "White v. Black people")))
 
-# Superiority Forest Plot ------------------------------------------------------
+# Generate forest plots for each dimension -------------------------------------
 
 # Change working directory to Plots
 setwd("../Plots")
 
-ggplot(superior_table, aes(x = effect, y = groups, xmin = lower, xmax = upper)) +
+superior_plot <- ggplot(superior_table, aes(x = effect, y = groups, xmin = lower, xmax = upper)) +
   geom_vline(xintercept = 0, linetype = "longdash") + 
   scale_x_continuous(limits = c(-1, 2.5),
                      breaks = c(-1.0, -0.5, 0, 0.5, 1.0, 1.5, 2.0, 2.5),
@@ -58,15 +59,7 @@ ggplot(superior_table, aes(x = effect, y = groups, xmin = lower, xmax = upper)) 
         axis.title.y = element_blank(),
         axis.text.y = element_text(size = 16, margin = margin(r = 5)))
 
-# Change below line of code to plot results using group word stimuli 
-# compiled using different threshold values
-# ggsave(file = "superior_weat_70.pdf", width = 10, height = 3, dpi = "retina")
-# ggsave(file = "superior_weat_60.pdf", width = 10, height = 3, dpi = "retina")
-ggsave(file = "superior_weat_7020.pdf", width = 10, height = 3, dpi = "retina")
-
-# Americanness Forest Plot -----------------------------------------------------
-
-ggplot(american_table, aes(x = effect, y = groups, xmin = lower, xmax = upper)) +
+american_plot <- ggplot(american_table, aes(x = effect, y = groups, xmin = lower, xmax = upper)) +
   geom_vline(xintercept = 0, linetype = "longdash") + 
   scale_x_continuous(limits = c(-1, 2.5),
                      breaks = c(-1.0, -0.5, 0, 0.5, 1.0, 1.5, 2.0, 2.5),
@@ -80,10 +73,13 @@ ggplot(american_table, aes(x = effect, y = groups, xmin = lower, xmax = upper)) 
         axis.title.y = element_blank(),
         axis.text.y = element_text(size = 16, margin = margin(r = 5)))
 
+# Place them on a single grid and label them -----------------------------------
+
+ggarrange(superior_plot, american_plot, nrow = 2, labels = c("A", "B"))
+
 # Change below line of code to plot results using group word stimuli 
 # compiled using different threshold values
-# ggsave(file = "american_weat_70.pdf", width = 10, height = 3, dpi = "retina")
-# ggsave(file = "american_weat_60.pdf", width = 10, height = 3, dpi = "retina")
-ggsave(file = "american_weat_7020.pdf", width = 10, height = 3, dpi = "retina")
-
+ggsave(file = "weat_70.pdf", width = 10, height = 6, dpi = "retina")
+# ggsave(file = "weat_60.pdf", width = 10, height = 6, dpi = "retina")
+# ggsave(file = "weat_7020.pdf", width = 10, height = 6, dpi = "retina")
 
